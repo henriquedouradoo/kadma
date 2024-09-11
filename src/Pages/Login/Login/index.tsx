0d476a147c40da145ccb3d845bfb06563af1aec4
Henrique Dourado Domingos <henriquedsenai@gmail.com>
2024-09-10 22:39:46 -0300

    Add circle com svg na tela de login

--- src/Pages/Login/Login/index.tsx
+++ src/Pages/Login/Login/index.tsx
@@ -14,7 +14,7 @@ export function Login() {
                 
                 <svg width="351" height="624" viewBox="0 0 351 624" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <ellipse cx="-51" cy="350" rx="402" ry="350" fill="#FFDD00"/>
-                <ellipse cx="-51" cy="350" rx="176.937" ry="154.049" fill="#161616"/>
+                <ellipse cx="-51" cy="350" rx="176.937" ry="154.049" fill="#101010"/>
                 </svg>
     
                 </div>
