9f339656b977ce4b576c6e4e9639d88bd92d64a2
henriquedouradoo <douradoprofissional@gmail.com>
2024-09-19 09:33:22 -0300

    Instalando clerk para iniciar, aplicacao de API

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,7 +3,7 @@ import { Login } from './Pages/Login/Login';
 import { Registro } from './Pages/Registro';
 import { Pricing } from './Pages/Pricing';
 import { Suporte } from './Pages/Suporte';
-import { Home } from './Pages/Home/Home';
+import { Home } from './Pages/Home';
 import { DefaultLayout } from './layout/DefaultLayout';
 
 export function Router() {
