a37b7a0532fa9e5630d4161a0b8202e68482d91e
henriquedouradoo <henriquedsenai@gmail.com>
2024-10-04 16:32:59 -0300

    add: img na testemunha

--- src/components/Testemunha/index.tsx
+++ src/components/Testemunha/index.tsx
@@ -1,4 +1,5 @@
 import './styles.css';
+import Deleitte from '../../Assets/deleitte.png';
 
 export function Testemunha() {
     return (
@@ -7,6 +8,7 @@ export function Testemunha() {
             <div className="text">
                 <h5>“kadma is a great product! All of my most important information is there - dashboards, business, process automations, and more. And I don't need to worry because it's all in one place! thanks!”</h5>
             </div>
+           <img src={Deleitte} />
             </div>
         </div>
     )
