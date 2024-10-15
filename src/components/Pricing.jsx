85594159f40f16f19c6f034eab022c2795bc231e
Henrique Dourado Domingos <henriquedsenai@gmail.com>
2024-10-14 23:03:42 -0300

    alterando hover do componente pricing

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -14,7 +14,7 @@ const Pricing = () => {
               <p className="text-4xl mb-8">
                 {option.title}
                 {option.title === "Pro" && (
-                  <span className="bg-gradient-to-r from-orange-500 to-red-400 text-transparent bg-clip-text text-xl mb-4 ml-2">
+                  <span className="bg-gradient-to-r from-yellow-500 to-yellow-400 text-transparent bg-clip-text text-xl mb-4 ml-2">
                     (Most Popular)
                   </span>
                 )}
@@ -33,7 +33,7 @@ const Pricing = () => {
               </ul>
               <a
                 href="#"
-                className="inline-flex justify-center items-center text-center w-full h-12 p-5 mt-20 tracking-tight text-xl hover:bg-yellow-900 border border-yellow-900 rounded-lg transition duration-200"
+                className="inline-flex justify-center items-center text-center w-full h-12 p-5 mt-20 tracking-tight text-xl hover:bg-yellow-500 border border-yellow-600 rounded-lg transition duration-200"
               >
                 Subscribe
               </a>
